1eb38af5ec28a8c12a7a73828723ea3850db4566
abstract-animation-9914 <vm930@hotmail.com>
2019-01-16 18:22:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.length !== 0
   name_hash.each {|name,num|
-  if num < num -1
+   
+    num < num -1
     name
   elsif num == 0
     nil
   end
   }
+ end 
 end
